2436ee5561119c46a587e74ce2715463fda730c1
unknown <tefihvmoonwalker77746@gmail.com>
2023-04-25 12:32:23 -0400

    msj

---
+++ resources/views/layouts/partials/nav.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">
+    <img src="https://openfs.us/img/DigitalMarketingAgencyNewJersey-MarketingDigitalNewJersey-PerformanceMarketingCompany-SearchEngineOptimization-SEO-SEM-DigitalAgency-NJ-ConversionRateOptimization-cro-GrowthHackingB2B-logo.svg" alt="Logo" width="130" height="54" class="d-inline-block align-text-top">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Home</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    </head>
+    <body>
+        @include('layouts.partials.nav')
+        
+        <div class="img">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrkMaqJ6MboQ5Tndtw91vrEgpRRy5GlYZRph9gZuaFkw&s" class="img-fluid" alt="..." width="100%">
+        </div>
+        <section class="services">
+            <div class="container text-center">
+            <h2>Our services</h2>
+            <h3>We are specialized on</h3>
+            </div>
+            <div class="container">
+                <div class="row justify-content-evenly">
+                    <img class="col-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrkMaqJ6MboQ5Tndtw91vrEgpRRy5GlYZRph9gZuaFkw&s" alt="SEO" aria_label="Search Engine Optimization">
+                    <img class="col-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrkMaqJ6MboQ5Tndtw91vrEgpRRy5GlYZRph9gZuaFkw&s" alt="SEM" aria_label="Search Engine Maketing">
+                </div>
+                <br><br>
+                <div class="row justify-content-evenly">
+                    <img class="col-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrkMaqJ6MboQ5Tndtw91vrEgpRRy5GlYZRph9gZuaFkw&s" alt="WEB" aria_label="Web Development">
+                    <img class="col-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrkMaqJ6MboQ5Tndtw91vrEgpRRy5GlYZRph9gZuaFkw&s" alt="CRO" aria_label="Web Development">
+                </div>
+            </div>
+        </section>
+        <section class="company">
+                <div class="container text-center">
+                    <h2>what we can do for your company</h2>
+                </div>
+                <div class="row justify-content-between">
+                    <p class="col-lg-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio architecto quae voluptatibus recusandae dignissimos incidunt totam, cum molestiae blanditiis quis accusantium eligendi aut illo obcaecati pariatur? Impedit, quaerat ea. Expedita.
+                    Non odio necessitatibus suscipit, vero, dolorem earum animi nostrum cupiditate, similique corporis deserunt itaque quae iusto nesciunt et hic ipsum quam ea debitis aliquam harum voluptates nulla quidem. Unde, illum!
+                    Non odio necessitatibus suscipit, vero, dolorem earum animi nostrum cupiditate, similique corporis deserunt itaque quae iusto nesciunt et hic ipsum quam ea debitis aliquam harum voluptates nulla quidem. Unde, illum!
+                    Non odio necessitatibus suscipit, vero, dolorem earum animi nostrum cupiditate, similique corporis deserunt itaque quae iusto nesciunt et hic ipsum quam ea debitis aliquam harum voluptates nulla quidem. Unde, illum!
+                    </p>
+                    <p class="col-lg-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio architecto quae voluptatibus recusandae dignissimos incidunt totam, cum molestiae blanditiis quis accusantium eligendi aut illo obcaecati pariatur? Impedit, quaerat ea. Expedita.
+                    Non odio necessitatibus suscipit, vero, dolorem earum animi nostrum cupiditate, similique corporis deserunt itaque quae iusto nesciunt et hic ipsum quam ea debitis aliquam harum voluptates nulla quidem. Unde, illum!
+                    Non odio necessitatibus suscipit, vero, dolorem earum animi nostrum cupiditate, similique corporis deserunt itaque quae iusto nesciunt et hic ipsum quam ea debitis aliquam harum voluptates nulla quidem. Unde, illum!
+                    Non odio necessitatibus suscipit, vero, dolorem earum animi nostrum cupiditate, similique corporis deserunt itaque quae iusto nesciunt et hic ipsum quam ea debitis aliquam harum voluptates nulla quidem. Unde, illum!
+                    </p>
+                </div>
+        </section>
+        <section>
+        <div class="container">
+            <div class="row justify-content-evenly">
+            <div class="card" style="width: 18rem;">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrkMaqJ6MboQ5Tndtw91vrEgpRRy5GlYZRph9gZuaFkw&s" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">Card</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's      content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrkMaqJ6MboQ5Tndtw91vrEgpRRy5GlYZRph9gZuaFkw&s" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">Card</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's      content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrkMaqJ6MboQ5Tndtw91vrEgpRRy5GlYZRph9gZuaFkw&s" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">Card</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's      content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            </div>
+            </div>
+        </section>
+    </body>
+</html>
